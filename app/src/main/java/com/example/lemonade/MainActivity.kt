112f604caf95a4d1d56dafb725f2d9c6bc72905a
haeskim <govkflek@naver.com>
2024-12-17 21:32:04 +0900

    이미지를 버튼화 하고 버튼 클릭에 따른 동작 완성

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -5,18 +5,30 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonDefaults.shape
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -24,8 +36,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.lemonade.ui.theme.LemonadeTheme
@@ -50,97 +64,159 @@ fun LemonadeApp() {
         .wrapContentSize(Alignment.Center))
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LemonadeButtonAndImage(modifier: Modifier = Modifier) {
 
     var pageNumber by remember { mutableStateOf(1)}
 
-    var squeezeTime by remember { mutableStateOf(1) }
-    squeezeTime = (2..4).random()
+    var squeezeTime by remember { mutableStateOf(0) }
 
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
-        when (pageNumber) {
-            1 -> {
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    Image(
-                        painter = painterResource(R.drawable.lemon_tree),
-                        contentDescription = stringResource(R.string.lemon_tree),
-                        modifier = Modifier
-                            .wrapContentSize()
-                            .clickable {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = "Lemonade",
+                        fontWeight = FontWeight.Bold
+                    )
+                },
+                colors = TopAppBarDefaults.largeTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer
+                )
+            )
+        }
+    ) { innerPadding->
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .background(MaterialTheme.colorScheme.tertiaryContainer),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            when (pageNumber) {
+                1 -> {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Button(
+                            onClick = {
                                 pageNumber = 2
-                            }
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Text(
-                        text = stringResource(R.string.first_image)
-                    )
+                                squeezeTime = (2..4).random()
+                            },
+                            shape = RoundedCornerShape(40.dp),
+                            colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.tertiaryContainer)
+                        ) {
+                            Image(
+                                painter = painterResource(R.drawable.lemon_tree),
+                                contentDescription = stringResource(R.string.lemon_tree),
+                                modifier = Modifier
+                                    .width(128.dp)
+                                    .height(160.dp)
+                                    .padding(24.dp)
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(32.dp))
+                        Text(
+                            text = stringResource(R.string.first_image),
+                            style = MaterialTheme.typography.bodyLarge
+                        )
+                    }
                 }
-            }
-            2 -> {
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    Image(
-                        painter = painterResource(R.drawable.lemon_squeeze),
-                        contentDescription = stringResource(R.string.lemon),
-                        modifier = Modifier
-                            .wrapContentSize()
-                            .clickable {
-                                pageNumber = 3/*TODO*/
-                            }
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Text(
-                        text = stringResource(R.string.second_image)
-                    )
+
+                2 -> {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Button(
+                            onClick = {
+                                squeezeTime--
+                                if (squeezeTime == 0) {
+                                    pageNumber = 3
+                                }
+                            },
+                            shape = RoundedCornerShape(40.dp),
+                            colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.tertiaryContainer)
+                        ) {
+                            Image(
+                                painter = painterResource(R.drawable.lemon_squeeze),
+                                contentDescription = stringResource(R.string.lemon),
+                                modifier = Modifier
+                                    .width(128.dp)
+                                    .height(160.dp)
+                                    .padding(24.dp)
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(32.dp))
+                        Text(
+                            text = stringResource(R.string.second_image),
+                            style = MaterialTheme.typography.bodyLarge
+                        )
+                    }
                 }
-            }
-            3 -> {
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    Image(
-                        painter = painterResource(R.drawable.lemon_drink),
-                        contentDescription = stringResource(R.string.glass_of_lemonade),
-                        modifier = Modifier
-                            .wrapContentSize()
-                            .clickable {
+
+                3 -> {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Button(
+                            onClick = {
                                 pageNumber = 4
-                            }
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Text(
-                        text = stringResource(R.string.third_image)
-                    )
+                            },
+                            shape = RoundedCornerShape(40.dp),
+                            colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.tertiaryContainer)
+                        ) {
+                            Image(
+                                painter = painterResource(R.drawable.lemon_drink),
+                                contentDescription = stringResource(R.string.glass_of_lemonade),
+                                modifier = Modifier
+                                    .width(128.dp)
+                                    .height(160.dp)
+                                    .padding(24.dp)
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(32.dp))
+                        Text(
+                            text = stringResource(R.string.third_image),
+                            style = MaterialTheme.typography.bodyLarge
+                        )
+                    }
                 }
-            }
-            4 -> {
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    Image(
-                        painter = painterResource(R.drawable.lemon_restart),
-                        contentDescription = stringResource(R.string.empty_glass),
-                        modifier = Modifier
-                            .wrapContentSize()
-                            .clickable {
+
+                4 -> {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Button(
+                            onClick = {
                                 pageNumber = 1
-                            }
-                    )
+                            },
+                            shape = RoundedCornerShape(40.dp),
+                            colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.tertiaryContainer)
+                        ) {
+                            Image(
+                                painter = painterResource(R.drawable.lemon_restart),
+                                contentDescription = stringResource(R.string.empty_glass),
+                                modifier = Modifier
+                                    .width(128.dp)
+                                    .height(160.dp)
+                                    .padding(24.dp)
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(32.dp))
+                        Text(
+                            text = stringResource(R.string.last_image),
+                            style = MaterialTheme.typography.bodyLarge
+                        )
+                    }
                 }
             }
         }
